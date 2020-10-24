256130c9c374b49e7813401857298fe9a3abb0dd
cjp257 <190674000@qq.com>
2020-10-24 14:30:29 +0800

    第四次修改

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h2>预祝大家前程似锦</h2>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603444352323&di=cb91bede6cfa10c179877910bd27847e&imgtype=0&src=http%3A%2F%2Fp0.itc.cn%2Fimages01%2F20201019%2F444b2ec6d9464f29ab8f94a0b21ca5cb.jpeg"/>
     <h2>G2加油！six six six </h2>
-    <h2>天降正义！ </h2>
+    <h2 style="color:red">天降正义！ </h2>
     <h2>狡诈恶徒，正义必胜！ </h2>
     <h2>我将带头冲锋！ </h2>
   </div>
